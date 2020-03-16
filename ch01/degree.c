a5c106c47fbc40f2015d9b8728bbf4f594c3d861
takkyuuplayer <takkyuuplayer@gmail.com>
2020-03-16 19:03:22 -0400

    chap 1.3

--- ch01/degree.c
+++ ch01/degree.c
@@ -2,18 +2,12 @@
 
 int main() {
     float fahr, celcius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
     printf("| %3s | %6s |\n", "F", "C");
     printf("|--------------|\n");
-    while(fahr <= upper) {
+    for (fahr = 300; fahr >= 0; fahr -= 20)
+    {
         celcius = (5.0 / 9.0) * (fahr - 32 );
         printf("| %3.0f | %6.1f |\n", fahr, celcius);
-        fahr = fahr + step;
     }
 }
