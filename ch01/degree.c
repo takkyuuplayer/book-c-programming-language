1aa1809ddb51f8e97ec100dafd4190b0fd9632cd
takkyuuplayer <takkyuuplayer@gmail.com>
2020-03-16 19:07:01 -0400

    ch 1.4

--- ch01/degree.c
+++ ch01/degree.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+# define LOWER 0
+# define UPPER 300
+# define STEP 20
+
 int main() {
     float fahr, celcius;
 
     printf("| %3s | %6s |\n", "F", "C");
     printf("|--------------|\n");
-    for (fahr = 300; fahr >= 0; fahr -= 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
     {
         celcius = (5.0 / 9.0) * (fahr - 32 );
         printf("| %3.0f | %6.1f |\n", fahr, celcius);
