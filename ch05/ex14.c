1d5d209c14b928c88194b4a22b905c9af51ee8a5
takkyuuplayer <takkyuuplayer@gmail.com>
2020-04-12 14:15:21 -0400

    Add ex5.14

---
+++ ch05/ex14.c
@@ -0,0 +1,155 @@
+// Exercise 5-14. Modify the sort program to handle a -r flag, which indicates sorting in reverse (decreasing) order. Be sure that -r works with -n.
+// Kernighan, Brian W.. C Programming Language (p. 121). Pearson Education. Kindle Edition.
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
+
+int _readlines(char *lineptr[], int nlines);
+void _writelines(char *lineptr[], int nlines);
+
+void _qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
+void _reverse(void *lineptr[], int size);
+int _numcmp(const char *, const char *);
+void swap(void *v[], int i, int j);
+
+int main(int argc, char const *argv[])
+{
+    int nlines;
+    int numeric = 0;
+    int reverse = 0;
+
+    while (--argc > 0)
+    {
+        ++argv;
+        if (strcmp(*argv, "-n") == 0)
+        {
+            numeric = 1;
+        }
+        else if (strcmp(*argv, "-r") == 0)
+        {
+            reverse = 1;
+        }
+    }
+    if (argc > 1 && strcmp(argv[1], "-n") == 0)
+    {
+        numeric = 1;
+    }
+
+    if ((nlines = _readlines(lineptr, MAXLINES)) >= 0)
+    {
+        _qsort((void **)lineptr, 0, nlines - 1,
+               (int (*)(void *, void *))(numeric ? _numcmp : strcmp));
+        if (reverse)
+        {
+            _reverse((void **)lineptr, nlines - 1);
+        }
+        _writelines(lineptr, nlines);
+        return 0;
+    }
+    printf("input too big to sort\n");
+    return -1;
+}
+
+void _qsort(void *v[], int left, int right, int (*comp)(void *, void *))
+{
+    int i, last;
+    void swap(void *v[], int, int);
+    if (left >= right) /* do nothing if array contains */
+        return;        /* fewer than two elements */
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    _qsort(v, left, last - 1, comp);
+    _qsort(v, last + 1, right, comp);
+}
+void _reverse(void *v[], int size)
+{
+    for (int i = 0; i < size / 2; i++)
+    {
+        swap(v, i, size - i);
+    }
+}
+
+#include <stdlib.h>
+/* numcmp:  compare s1 and s2 numerically */
+int _numcmp(const char *s1, const char *s2)
+{
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
+}
+
+void swap(void *v[], int i, int j)
+{
+    void *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+#define MAXLEN 1000 /* max length of any input line */
+int _getline(char *, int);
+char *alloc(int);
+
+int _readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+    nlines = 0;
+    while ((len = _getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else
+        {
+            line[len - 1] = '\0'; /* delete newline */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+int _getline(char s[], int lim)
+{
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void _writelines(char *lineptr[], int nlines)
+{
+    int i;
+    for (i = 0; i < nlines; i++)
+        printf("%s\n", lineptr[i]);
+}
+
+#define ALLOCSIZE 10000          /* size of available space */
+static char allocbuf[ALLOCSIZE]; /* storage for alloc */
+static char *allocp = allocbuf;  /* next free position */
+char *alloc(int n)               /* return pointer to n characters */
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n)
+    { /* it fits */
+        allocp += n;
+        return allocp - n; /* old p */
+    }
+    else /* not enough room */
+        return 0;
+}
